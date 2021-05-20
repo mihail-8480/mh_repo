2f2fbedc727e5ddd76c86118e80cfd697b8e7c2a
mihail-8480 <mihail.mojsoski@outlook.com>
2021-05-20 15:19:08 +0200

    Fixed clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for d in */ ; do
     find "$d" ! -name 'PKGBUILD' -type f -exec rm -f {} +
-    find "$d" ! -name "$d" -type d -exec rm -rf {} +
+    find "$d" ! -name "$(echo "$d" | sed 's:/*$::')" -type d -exec rm -rf {} +
 done
