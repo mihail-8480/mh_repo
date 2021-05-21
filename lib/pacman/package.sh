2e16bf42ca0923dd91bac2be0ee3d1e0fed88704
mihail-8480 <mihail-mojsoski@outlook.com>
2021-05-21 16:20:39 +0200

    Added 'add_dependency'.

--- lib/pacman/package.sh
+++ lib/pacman/package.sh
@@ -1,22 +1,16 @@
-setup_git() {
-  export MH_GIT_SERVER=${1}
-  export MH_GIT_USERNAME=${2}
-}
+source "$MH_PKG_PATH/lib/generic.sh"
 
 setup_package() {
   pkgname=${1}
   pkgdesc=${2}
   pkgver=${3}
+  depends=()
+}
+
+add_dependency() {
+  depends+=(${1})
 }
 
 use_cmake() {
   source "$MH_PKG_PATH/lib/pacman/cmake.sh"
-}
-
-mh_git_version() {
-  curl -s https://raw.githubusercontent.com/mihail-8480/mh/master/CMakeLists.txt | grep "VERSION " | sed -n 2p | xargs | cut  -d' ' -f2
-}
-
-mh_version() {
-  mh_git_version || echo 0.0.0
 }
